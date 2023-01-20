3fabc389c5b5a8cfe002e0efd181587a4aaee355
Gary Bleasdale <gary@bleasdale.dev>
2023-01-20 16:44:36 -0300

    enabled random meme image generation from a static database

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,31 @@
+import memeData from '../assets/memeData'
+import {useState} from 'react'
+
+
 export default function Input (){
+    
+    const [imgUrl, setImgUrl] = useState("")
+
+    function imageGetter(){
+        let randomNum = Math.floor(Math.random() * 99)
+        let randomImg =  memeData.data.memes[randomNum].url 
+        setImgUrl(randomImg)
+     } 
+
+
+
     return (
         <div>
-            <form className='py-8 mx-auto w-[90vw] max-w-5xl'>
+            <div className='py-8 mx-auto w-[90vw] max-w-5xl'>
                 <div className='flex justify-between'>
-                <input className=' w-1/2 mr-4 p-2 border border-slate-400 rounded-lg placeholder:text-slate-600'type="text" placeholder="What do you want to meme?"/> 
-                <input className='w-1/2 ml-4 p-2 border border-slate-400 rounded-lg placeholder:text-slate-600' type="text" placeholder="meme some more"/>     
+                    <input className=' w-1/2 mr-4 p-2 border border-slate-400 rounded-lg placeholder:text-slate-600'type="text" placeholder="Top Text"/> 
+                    <input className='w-1/2 ml-4 p-2 border border-slate-400 rounded-lg placeholder:text-slate-600' type="text" placeholder="Bottom text"/>     
                 </div>    
-                <button className='w-full my-6 bg-slate-600 py-4 text-white font-bold rounded-lg bg-gradient-to-r from-fuchsia-900 to-purple-600' type="submit"> Get a new meme image</button>
-            </form>
+                <button className='w-full my-6 bg-slate-600 py-4 text-white font-bold rounded-lg bg-gradient-to-r from-fuchsia-900 to-purple-600' type="button" onClick={imageGetter}> Get a new meme image</button>
+            </div>
+            <div >
+                <img src={imgUrl} className='m-auto'/>
+            </div>
         </div>
     )
 }
